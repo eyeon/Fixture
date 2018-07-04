85c04c1d8d71f84c30349bb7cf19f1cbbfdfc03c
Kuntal Majumder <hellozee@disroot.org>
2018-07-04 23:44:14 +0530

    Layers switch the correct upon switching tabs

--- src/widgets/layermanager.cpp
+++ src/widgets/layermanager.cpp
@@ -1,8 +1,11 @@
 #include "layermanager.h"
 
+#include <QDebug>
+
 LayerManager::LayerManager(QWidget *parent):
     QListWidget(parent)
 {
+    setDragDropMode(QAbstractItemView::InternalMove);
 }
 
 LayerManager::~LayerManager()
@@ -11,10 +14,14 @@ LayerManager::~LayerManager()
 
 void LayerManager::updateItems(QList<Layer> items)
 {
-    _curItems = items;
-    QList<Layer>::iterator itr = _curItems.begin();
-    for(int i=0;itr != _curItems.end();++itr,i++){
+    int max = count();
+    for(int i=max-1;i>=0;i--){
         takeItem(i);
+    }
+
+    _curItems = items;
+    QList<Layer>::reverse_iterator itr = _curItems.rbegin();
+    for(int i=0;itr != _curItems.rend();++itr,i++){
         insertItem(i,itr->getListItem());
     }
 }
