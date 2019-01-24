0de66fe911f323c2cfbc32e2adc8712c419c3c57
Kuntal Majumder <hellozee@disroot.org>
2019-01-24 20:57:33 +0530

    cleanup, preparing for the big canvas rewrite from scratch

--- src/tools/abstractperception.cpp
+++
@@ -1,4 +0,0 @@
-#include "abstractperception.h"
-
-AbstractPerception::AbstractPerception()
-{ }
--- src/tools/abstractselection.cpp
+++
@@ -1,7 +0,0 @@
-#include "abstractselection.h"
-
-AbstractSelection::AbstractSelection(QIcon ico, const QString &name,
-                                     QCursor cur, ToolGroup group,
-                                     ToolType type, QWidget *parent)
-    : Tool(ico, name, cur, group, type, parent), _scene(nullptr)
-{ }
